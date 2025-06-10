4d28e2da88f42730120e8e6abecdfefbe87f8a8e
mysinglepixel <mysinglepixel@gmail.com>
2025-06-10 12:37:35 +0800

    skin mask

--- new try/main.py
+++ new try/main.py
@@ -3,3 +3,5 @@
 # we need to judge each point whether it is in one of the polygon and classfy them to different part 
 # one way is to divide to five matrices, if one pixel is in hair area, change it to 1
 # send hair mask and origin picture to unet, make it return preview mask
+# the skin mask should cover the hair mask
+
